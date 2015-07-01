3f9003268d0cf40d375fcffef5e1ccca6268bffb
OJIMA Ryoji <rojima1@gmail.com>
2015-07-01 15:23:12 +0900

    Some tests are added.

--- test/on_lisp_with_clojure/core_test.clj
+++ test/on_lisp_with_clojure/core_test.clj
@@ -40,7 +40,6 @@
          (if-match [nil & []] [] :matched :not-matched))))
 
 (deftest test-with-rules-and-query
-  
   (with-rules (-> *rules*
                   (<- (append [] Xs Xs))
                   (<- (append [X & As] Bs [X & Cs])
@@ -213,6 +212,139 @@
     (is (= [8]
            (with-inference (最大値 [2 4 6 8 3] V)
              V))))
+
+  (let [rules-0 (-> *rules*
+                    (<- (全ての要素が同じ [] _))
+                    (<- (全ての要素が同じ [X & Xs] X)
+                        (全ての要素が同じ Xs X)))
+        rules-1 (-> rules-0
+                    (<- (append [] Xs Xs))
+                    (<- (append [X & As] Bs [X & Cs])
+                        (append As Bs Cs)))
+        rules-2 (-> rules-1
+                    (<- (全ての要素が同じ-但し空リストは除く L A)
+                        (全ての要素が同じ L A)
+                        (not (clj (= L [])))))]
+    (with-rules rules-0
+      (is (= []
+             (with-inference (全ての要素が同じ [1 2 1] X)
+               X)))
+      (is (= [1]
+             (with-inference (全ての要素が同じ [1 1 1] X)
+               X)))
+      (is (= [[1 1 1]]
+             (with-inference (全ての要素が同じ [X Y Z] 1)
+               [X Y Z])))
+      (is (= [[1 1 1]]
+             (with-inference (全ての要素が同じ [A 1 B] X)
+               [A B X]))))
+    (with-rules rules-1
+      (is (= []
+             (first (with-inference (and (append L1 L2 [1 1 3 2]) (全ての要素が同じ L1 A))
+                      L1)))))
+    (with-rules rules-2
+      (is (= [1]
+             (first (with-inference (and (append L1 L2 [1 1 3 2]) (全ての要素が同じ-但し空リストは除く L1 A))
+                      L1))))))
+
+  (with-rules (-> *rules*
+                  (<- (最大公約数 N ?最大公約数 ?最大公約数)
+                      (not (clj (= ?最大公約数 0)))
+                      (clj (= 0 (mod N ?最大公約数))))
+                  (<- (最大公約数 N M ?最大公約数)
+                      (not (clj (= M 0)))
+                      (is M_2 (mod N M))
+                      (最大公約数 M M_2 ?最大公約数)))
+    (is (= [7]
+           (with-inference (最大公約数 49 28 X)
+             X))))
+
+  (with-rules (-> *rules*
+                  (<- (エラトステネスの篩 [] [])
+                      (cut))
+                  (<- (エラトステネスの篩 [A & R1] [A & R2])
+                      (エラトステネスの篩 A R1 L)
+                      (エラトステネスの篩 L R2))
+                  (<- (エラトステネスの篩 _ [] [])
+                      (cut))
+                  (<- (エラトステネスの篩 N [A & R1] R2)
+                      (clj (= 0 (mod A N)))
+                      (エラトステネスの篩 N R1 R2)
+                      (cut))
+                  (<- (エラトステネスの篩 N [A & R1] [A & R2])
+                      (エラトステネスの篩 N R1 R2)))
+    (let [xs (nnext (range 32))]
+      (is (= [[2 3 5 7 11 13 17 19 23 29 31]]
+             (with-inference (エラトステネスの篩 xs Xs)
+               Xs)))))
+  
+  (with-rules (-> *rules*
+                  (<- (append [] L1 L1))
+                  (<- (append [X & L1] L2 [X & L3])
+                      (append L1 L2 L3))
+                  (<- (append [] L2 L3 L4)
+                      (append L2 L3 L4))
+                  (<- (append [X & L1] L2 L3 [X & L4])
+                      (append L1 L2 L3 L4)))
+    (is (= [[[] :a :b [:c :d]]
+            [[:a] :b :c [:d]]
+            [[:a :b] :c :d []]]
+           (with-inference (append L1 [X Y] L3 [:a :b :c :d])
+             [L1 X Y L3]))))
+  
+  (with-rules (-> *rules*
+                  (<- (append [] L1 L1))
+                  (<- (append [X & L1] L2 [X & L3])
+                      (append L1 L2 L3))
+                  (<- (リスト要素の隣 ?リスト ?要素 ?隣の要素)
+                      (append _ [?隣の要素 ?要素 & _] ?リスト))
+                  (<- (リスト要素の隣 ?リスト ?要素 ?隣の要素)
+                      (append _ [?要素 ?隣の要素 & _] ?リスト))
+                  (<- (リスト要素の両隣 ?リスト ?要素 ?隣の要素_1 ?隣の要素_2)
+                      (append _ [?隣の要素_1 ?要素 ?隣の要素_2 & _] ?リスト))
+                  (<- (リスト要素の両隣 ?リスト ?要素 ?隣の要素_1 ?隣の要素_2)
+                      (append _ [?隣の要素_2 ?要素 ?隣の要素_1 & _] ?リスト)))
+    (is (= [[:b :a] [:c :b] [:a :b] [:b :c]]
+           (with-inference (リスト要素の隣 [:a :b :c] X Y)
+             [X Y])))
+    (is (= []
+           (with-inference (リスト要素の隣 [:a :b :c :d :e :f :g] :b :f)
+             :matched)))
+    (is (= [:matched]
+           (with-inference (リスト要素の隣 [:a :b :c :d :e :f :g] :e :d)
+             :matched)))
+    (is (= [[:d :f] [:f :d]]
+           (with-inference (リスト要素の両隣 [:a :b :c :d :e :f :g] :e Y Z)
+             [Y Z]))))
+
+  (with-rules (-> *rules*
+                  (<- (append [] L1 L1))
+                  (<- (append [X & L1] L2 [X & L3])
+                      (append L1 L2 L3))
+                  (<- (リスト要素の反転 [] []))
+                  (<- (リスト要素の反転 [A & R1] L)
+                      (リスト要素の反転 R1 L2)
+                      (append L2 [A] L)))
+    (is (= [[:c :b :a]]
+           (with-inference (リスト要素の反転 [:a :b :c] L)
+             L))))
+
+  (with-rules (-> *rules*
+                  (<- (member X [X & _]))
+                  (<- (member X [_ & Xs])
+                      (member X Xs))
+                  (<- (組合せ X 1 [A])
+                      (member A X))
+                  (<- (組合せ [A & Y] N [A & X])
+                      (clj (> N 1))
+                      (is N_1 (- N 1))
+                      (組合せ Y N_1 X))
+                  (<- (組合せ [_ & Y] N A)
+                      (clj (> N 1))
+                      (組合せ Y N A)))
+    (is (= [[:a :b :c] [:a :b :d] [:a :c :d] [:b :c :d]]
+           (with-inference (組合せ [:a :b :c :d] 3 X)
+             X))))
   
   ;; TODO: Add more!
   )
