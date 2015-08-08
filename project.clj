2e3e232f9a39bcc112f9cf49ce655965e015b2b2
OJIMA Ryoji <rojima1@gmail.com>
2015-08-08 20:07:57 +0900

    Clojure version is updated.

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url          "https://github.com/tail-island/on-lisp-with-clojure"
   :license      {:name "Eclipse Public License"
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
